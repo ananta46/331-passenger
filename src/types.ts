293c26d2311adedf3c46bfc4e377c630ab1dc9ef
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-08-06 19:57:41 +0700

    First commit

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export interface Airline {
+    _id: string
+    name: string
+    country: string
+    logo: string
+    slogan: string
+    head_quaters: string
+    website: string
+    established: string
+}
+export interface passengers {
+    id: number
+    category: string
+    title: string
+    description: string
+    location: string
+    date: string
+    time: string
+    petsAllowed: boolean
+    organizer: string
+  }
